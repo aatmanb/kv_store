c661e756a59e14c4d77c3f841c5a9e2e3d652d19
AATMAN Borda <aatmanb@barolo.cs.wisc.edu>
2024-09-25 13:02:09 -0500

    working 1 client-server connection

---
+++ src/client.cc
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstring>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
+#include <grpcpp/grpcpp.h>
+#include "kv_store.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
+
+const uint16_t key_max_len = 128;
+const uint16_t value_max_len = 2048;
+
+// Function to convert std::string to modifiable char*
+char* stringToCharArray(const std::string& str) {
+    // Allocate memory for the char array (+1 for the null terminator)
+    char* charArray = new char[str.length() + 1];
+    
+    // Copy the contents of the string into the char array
+    std::strcpy(charArray, str.c_str());
+    
+    // Return the modifiable char array
+    return charArray;
+}
+
+// Function to convert a modifiable char array to std::string
+std::string charArrayToString(char* charArray) {
+    // Create a std::string from the char array
+    return std::string(charArray);
+}
+
+class client {
+    public:
+        client(std::shared_ptr<Channel> channel) : stub_(kv_store::NewStub(channel)) {}
+
+        int kv739_init(char *server_name) {
+            //TODO
+            return 0;
+        }
+
+        int kv739_shutdown() {
+            //TODO
+            return 0;
+        }
+
+        int kv739_get(char *key, char *value) {
+            //TODO
+            std::string key_str = charArrayToString(key);
+            std::cout << "client: get() called with key: " << key_str << std::endl;
+            getReq request;
+            request.set_key(key_str);
+
+            getResp response;
+
+            ClientContext context;
+
+            Status status = stub_->get(&context, request, &response);
+            std::strcpy(value, stringToCharArray(response.value()));
+            std::cout << "client: response for get" << "(" << key << ")" << ": " << value << std::endl;
+            return response.status();
+        }
+
+        int kv739_put(char *key, char *value, char *old_value) {
+            std::string key_str = charArrayToString(key);
+            std::string value_str = charArrayToString(value);
+            
+            std::cout << "client: put" << "(" << key_str << ")" << ": " << value_str << std::endl;
+            
+            putReq request;
+            request.set_key(key_str);
+            request.set_value(value_str);
+
+            putResp response;
+
+            ClientContext context;
+
+            Status status = stub_->put(&context, request, &response);
+            
+            std::strcpy(old_value, stringToCharArray(response.old_value()));
+            std::cout << "client: response for put" << "(" << key << ")" << ": " << old_value << std::endl;
+            return response.status();
+        }
+
+    private:
+        std::unique_ptr<kv_store::Stub> stub_;
+};
+
+void runGetTest(std::string target_str) {
+    std::cout << "----------- Start GetTest -------------" << std::endl;
+    char *test_key = "connection_test_key";
+    char *test_value = new char[value_max_len + 1];
+    int status = -1; // Error by default
+ 
+    // We indicate that the channel isn't authenticated (use of
+    // InsecureChannelCredentials()).
+    client _client(
+        grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+    
+    status = _client.kv739_get(test_key, test_value);
+    std::cout << "status: " << status << " " << "get" << "(" << test_key << ")" << ": " << test_value << std::endl;
+    std::cout << "----------- End GetTest -------------" << std::endl;
+}
+
+void runPutTest(std::string target_str) {
+    std::cout << "----------- Start PutTest -------------" << std::endl;
+    char *test_key = "connection_test_key";
+    char *test_value = "connection_test_value";
+    char *old_value = new char[value_max_len + 1];
+    int status = -1; // Error by default
+ 
+    // We indicate that the channel isn't authenticated (use of
+    // InsecureChannelCredentials()).
+    client _client(
+        grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+    
+    std::cout << "put" << "(" << test_key << ")" << ": " << test_value << std::endl;
+    status = _client.kv739_put(test_key, test_value, old_value);
+    std::cout << "status: " << status << " " << "put" << "(" << test_key << ")" << ": " << old_value << std::endl;
+    std::cout << "----------- End PutTest -------------" << std::endl;
+}
+
+
+int main(int argc, char** argv) {
+    absl::ParseCommandLine(argc, argv);
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint specified by
+    // the argument "--target=" which is the only expected argument.
+    std::string target_str = absl::GetFlag(FLAGS_target);
+   
+    runGetTest(target_str); 
+    runPutTest(target_str);
+
+    return 0;
+}
---
+++ src/server.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_format.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "cmake/build/kv_store.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
+
+class kv_storeImpl final : public kv_store::Service {
+	Status get(ServerContext* context, const getReq* request, getResp* response) override {
+		std::cout << "server finding value for key: " << request->key() << std::endl;
+		// TODO: assign the correct status
+		// 0 : key is present
+		// 1 : key is not present
+		// -1 : failure occured while querying
+        std::string test = "This is a test string. If seen, get() call was successful";
+        response->set_value(test);
+		response->set_status(0);
+		
+		return Status::OK;		
+	}
+	
+	Status put(ServerContext* context, const putReq* request, putResp* response) override {
+		std::cout << "server adding key to database: " << request->key() << std::endl;
+        std::string test = "This is a test string. If seen, put() call was successful";
+		response->set_old_value(test);
+        response->set_status(0);
+		return Status::OK;
+	}
+};
+
+void runServer(uint16_t port) {
+	std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+	kv_storeImpl service;
+	
+	grpc::EnableDefaultHealthCheckService(true);
+	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+	ServerBuilder builder;
+	// Listen on the given address without any authentication mechanism.
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	// Register "service" as the instance through which we'll communicate with
+	// clients. In this case it corresponds to an *synchronous* service.
+	builder.RegisterService(&service);
+	// Finally assemble the server.
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
+	
+	// Wait for the server to shutdown. Note that some other thread must be
+	// responsible for shutting down the server for this call to ever return.
+	server->Wait();
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  runServer(absl::GetFlag(FLAGS_port));
+  return 0;
+}
