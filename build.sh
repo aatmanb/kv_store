59fa0d411391d691f38c960c4ae675dc0419c4a5
AATMAN Borda <aatmanb@barolo.cs.wisc.edu>
2024-10-10 00:31:24 -0500

    Updated the directory structure and build infra

--- build.sh
+++ build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-cd ./src/cmake/build
-cmake -DCMAKE_PREFIX_PATH=$GRPC_INSTALL_DIR ../..
+ROOT_DIR=$(pwd)
+BUILD_DIR=$ROOT_DIR/build
+BIN_DIR=$ROOT_DIR/bin
+
+# Check if the build and bin directory exists, if not create it
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "Creating build directory $BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
+fi
+
+if [ ! -d "$BIN_DIR" ]; then
+    echo "Creating bin directory $BIN_DIR"
+    mkdir -p "$BIN_DIR"
+fi
+
+cd $BUILD_DIR
+cmake ..
 make -j 4
-cd ../../..
+cd $ROOT_DIR
